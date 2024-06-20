fa9dee0cda6f49eb5f54c80865813fcb2196707d
cattenlinger <lingerkong03@gmail.com>
2024-06-20 18:27:33 +0800

    Move common logger out to C.java

--- suit-lib/src/main/java/net/catten/cmdsuit/C.java
+++ suit-lib/src/main/java/net/catten/cmdsuit/C.java
@@ -1,7 +1,14 @@
 package net.catten.cmdsuit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class C {
     private C(){}
 
     public static final String LoggerName = C.class.getPackageName();
+
+    public static Logger getDefaultLogger() {
+        return LoggerFactory.getLogger(LoggerName);
+    }
 }
